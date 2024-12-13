9e2fe093e5767d188878c27295bbe6fc364a3183
Md. Robiul Islam <a02437860@aggies.usu.edu>
2024-12-13 09:51:02 -0700

    git command used so far
    This is a tutorial commands for git and github

---
+++ commands.py
@@ -0,0 +1,8 @@
+# git clone https://github.com/robiul02437860/demo-repo.git
+# dir 
+# cd demo-repo
+# dir /a
+# git status
+# git add .
+# git status
+# git commit -m "Added index" -m "Some description of inex file"
