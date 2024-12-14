b7790e2e2f7237b878b045b156cfbfff19e2fcc6
Md. Robiul Islam <a02437860@aggies.usu.edu>
2024-12-13 18:09:18 -0700

    Update commands.py

--- commands.py
+++ commands.py
@@ -9,3 +9,5 @@
 # git push origin main
 # git remote add origin https://github.com/robiul02437860/demo-repo2.git [for creating remote connection]
 # git remote -v [check remote connections]
+# git branch [check branch]
+# git push -u origin master [main will not work because locally it in master branch but github has renamed master branch to main branch]
